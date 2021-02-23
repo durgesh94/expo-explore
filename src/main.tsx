769158c5930ecccb0b702b61b39b629665486299
@durgesh.tambe <durgesh.tambe94@gmail.com>
2021-02-23 14:22:01 +0530

    init main screen

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const Main: React.FC = () => {
+
+    return (
+        <View style={{alignItems: "center"}}>
+            <Text>Main Screen</Text>
+        </View>
+    )
+}
