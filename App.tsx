c93f107eec03558fc5c9254218c4c928f02917a5
@durgesh.tambe <durgesh.tambe94@gmail.com>
2021-04-18 03:21:20 +0530

    init new-structure

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import MyStack from './src/routes';
+import MyStack from './src/navigations';
 
 const App: React.FC = () => <NavigationContainer><MyStack /></NavigationContainer>
 
